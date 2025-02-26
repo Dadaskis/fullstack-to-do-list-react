0185a3051b2cabd650b47e8620ce4c43f115bfa1
Dadaskis <37717199+Dadaskis@users.noreply.github.com>
2025-02-26 18:08:16 +0300

    Cleaning up some mess

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import TaskManager from "./TaskManager";
-import TodoList from "./Test";
 
 function App() {
-    try{
+    try {
         return (
             <div>
                 <TaskManager></TaskManager>
             </div>
         );
-    } catch(ex) {
+    } catch (ex) {
         console.error(ex);
-        <div>
-            <h1>
-                Unexpected client-side error has occured.
-            </h1>
-        </div>
+        return (
+            <div>
+                <h1>Unexpected client-side error has occured.</h1>
+            </div>
+        );
     }
 }
 
